8b2eec27175b61f5a5542f07012f73e08c6a1b98
Puskuruk <tamayeseruysal@gmail.com>
2019-03-13 01:29:21 +0300

    Updated Indentation

--- app/models/User.js
+++ app/models/User.js
@@ -5,10 +5,10 @@
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-	tokens			:		[String],
-	uname				:		{type: String},
-	pwd					:		{type: String},
-	createdDate :		{type: Date, default: Date.now}
+	tokens : [String],
+	uname : {type: String},
+	pwd : {type: String},
+	createdDate : {type: Date, default: Date.now}
 });
 
 mongoose.model('User', UserSchema);
